377ff70a03e4460617215119fe15e37ab3395727
Karen <karen2005yulieth@gmail.com>
2023-03-18 20:20:02 -0500

    Lab

--- src/RutaEnTiempo.java
+++ src/RutaEnTiempo.java
@@ -2,32 +2,45 @@ import java.util.Scanner;
 
 public class RutaEnTiempo {
     public static void main(String[] args) {
-        int cantidad=0;
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Ingrese la cantidad de coordenadas: ");
-        cantidad= sc.nextInt();
+        int cantidad = 0;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese la cantidad de coordenadas: ");
+            cantidad = sc.nextInt();
+        } while (cantidad < 1 || cantidad > 1000);
 
-        String[][] coordenadas = new String[cantidad][cantidad];
-        int xi; int yi;
+        int[] coordenadasX = new int[cantidad];
+        int[] coordenadasY = new int[cantidad];
+       int vc=0, vw=0; //vc es velocidad de cueva y vw es velocidad de la caminada
+        int temporal = 0;
 
-        for (int i = 0; i <cantidad ; i++) {
-            System.out.println("\nCoordenadas "+i);
-            System.out.println("Ingrese X ");
-            xi=sc.nextInt();
+        for (int i = 0; i < cantidad; i++) {
+            do {
+                do {
+                    System.out.println("\nCoordenadas " + i);
+                    System.out.println("Ingrese X ");
+                    coordenadasX[i] = sc.nextInt();
+                } while (coordenadasX[i] < coordenadasX[i - 1]);
+            } while (coordenadasX[i] < -10000 || coordenadasX[i] > 10000);
 
-            for (int j = 0; j <1 ; j++) {
+            do {
                 System.out.println("Ingrese y: ");
-                yi=sc.nextInt();
-                coordenadas[i][j]="("+xi+","+yi+")";
-            }
-
+                coordenadasY[i] = sc.nextInt();
+            } while (coordenadasY[i] < -10000 || coordenadasY[i] > 10000);
         }
-        for (int i = 0; i <cantidad ; i++) {
-            for (int j = 0; j <1 ; j++) {
-                System.out.println(coordenadas[i][j]);
-            }
 
-        }
+        //Ingresa la velocidad de la caminata normal
+        do {
+            System.out.println("Ingrese la velocidad de caminata: ");
+            vw=sc.nextInt();
+        }while(vw<1||vw>10);
+        //ingresa la velocidad de la cueva
+        do {
+            System.out.println("Ingrese la velocidad de la cueva: ");
+            vc=sc.nextInt();
+        }while(vc<1||vc>10);
+
+
 
     }
 
